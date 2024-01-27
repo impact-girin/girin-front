1fae1a0624a30348d61bfd586a28de92c374ef2d
kimgh06 <kkh061101@naver.com>
2024-01-27 17:37:50 +0900

    이미지 처리

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={inter.className}
+      // className={inter.className}
       >
         <Providers>{children}</Providers>
       </body>
