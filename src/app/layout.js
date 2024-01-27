0a4db297a22d370532035ad0531b54a63dd18cd7
kimgh06 <kkh061101@naver.com>
2024-01-27 17:22:04 +0900

    Let's 등산

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,9 +12,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body
+        className={inter.className}
+      >
         <Providers>{children}</Providers>
       </body>
-    </html>
+    </html >
   );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,9 +11,9 @@ export default function Home() {
   }, [])
   return (
     <Flex height={'70vh'} alignItems={'center'} justifyContent={'center'}>
-      <Text color={'#2DD790'} fontSize={'40px'} textAlign={'center'} fontWeight={'bold'}>
+      <Text color={'#2DD790'} fontSize={'40px'} textAlign={'center'} fontWeight={'900'}>
         우리 같이<br />
-        등산해요
+        등⛰️해요
       </Text>
     </Flex>
   );
