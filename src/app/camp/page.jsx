87fbf49aeff42cddeddbb8c5c3dad584bf5a3315
kimgh06 <kkh061101@naver.com>
2024-01-27 21:58:09 +0900

    back to the previous page

--- src/app/camp/page.jsx
+++ src/app/camp/page.jsx
@@ -19,70 +19,70 @@ const lists = [
     location: '서울/영등포구',
     title: '같이 북한산 가실 분 구함',
     headcount: 10,
-    img: null
+    img: 'https://velog.velcdn.com/images/kimgh06/profile/8dcc0c72-4a46-407a-8a8b-2bfd7c38d29a/image.png'
   },
   {
     id: '3',
     location: '서울/영등포구',
     title: '같이 북한산 가실 분 구함',
     headcount: 10,
-    img: null
+    img: 'https://velog.velcdn.com/images/kimgh06/profile/8dcc0c72-4a46-407a-8a8b-2bfd7c38d29a/image.png'
   },
   {
     id: '4',
     location: '서울/영등포구',
     title: '같이 북한산 가실 분 구함',
     headcount: 10,
-    img: null
+    img: 'https://velog.velcdn.com/images/kimgh06/profile/8dcc0c72-4a46-407a-8a8b-2bfd7c38d29a/image.png'
   },
   {
     id: '5',
     location: '서울/영등포구',
     title: '같이 북한산 가실 분 구함',
     headcount: 10,
-    img: null
+    img: 'https://velog.velcdn.com/images/kimgh06/profile/8dcc0c72-4a46-407a-8a8b-2bfd7c38d29a/image.png'
   },
   {
     id: '6',
     location: '서울/영등포구',
     title: '같이 북한산 가실 분 구함',
     headcount: 10,
-    img: null
+    img: 'https://velog.velcdn.com/images/kimgh06/profile/8dcc0c72-4a46-407a-8a8b-2bfd7c38d29a/image.png'
   },
   {
     id: '7',
     location: '서울/영등포구',
     title: '같이 북한산 가실 분 구함',
     headcount: 10,
-    img: null
+    img: 'https://velog.velcdn.com/images/kimgh06/profile/8dcc0c72-4a46-407a-8a8b-2bfd7c38d29a/image.png'
   },
   {
     id: '8',
     location: '서울/영등포구',
     title: '같이 북한산 가실 분 구함',
     headcount: 10,
-    img: null
+    img: 'https://velog.velcdn.com/images/kimgh06/profile/8dcc0c72-4a46-407a-8a8b-2bfd7c38d29a/image.png'
   },
   {
     id: '9',
     location: '서울/영등포구',
     title: '같이 북한산 가실 분 구함',
     headcount: 10,
-    img: null
+    img: 'https://velog.velcdn.com/images/kimgh06/profile/8dcc0c72-4a46-407a-8a8b-2bfd7c38d29a/image.png'
   },
   {
     id: '10',
     location: '서울/영등포구',
     title: '같이 북한산 가실 분 구함',
     headcount: 10,
-    img: null
+    img: 'https://velog.velcdn.com/images/kimgh06/profile/8dcc0c72-4a46-407a-8a8b-2bfd7c38d29a/image.png'
   },
   {
     id: '1',
     location: '서울/영등포구',
     title: '같이 북한산 가실 분 구함',
     headcount: 10,
-    img: null
+    img: 'https://velog.velcdn.com/images/kimgh06/profile/8dcc0c72-4a46-407a-8a8b-2bfd7c38d29a/image.png'
   },
 ]
 
@@ -95,7 +95,7 @@ export default function Home() {
       <Text textAlign={'center'} fontWeight={'bold'} fontSize={'20px'}>산악회 정보</Text>
     </Box>
     <Box>
-      <Flex onClick={e => navigate.push('#')} borderRadius={'20px'} padding={'2px 0 '} justifyContent={'center'} w={'90px'} fontSize={'14px'} color={'#2DD790'} margin={'16px'} marginBottom={'0'} style={{ border: '1px solid #2DD790' }}>
+      <Flex onClick={e => navigate.push('/camp/selectRegion')} borderRadius={'20px'} padding={'2px 0 '} justifyContent={'center'} w={'90px'} fontSize={'14px'} color={'#2DD790'} margin={'16px'} marginBottom={'0'} style={{ border: '1px solid #2DD790' }}>
         <svg width="14" height="20" viewBox="0 0 14 16" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M7.96295 14.559C9.41572 13.0226 11.9998 9.88522 11.9998 7.00649C11.9998 3.6892 9.76126 1 6.99984 1C4.23841 1 1.99984 3.6892 1.99984 7.00649C1.99984 9.88522 4.58396 13.0226 6.03673 14.559C6.59272 15.147 7.40696 15.147 7.96295 14.559ZM5.33317 7.00649C5.33317 5.90073 6.07936 5.00433 6.99984 5.00433C7.92031 5.00433 8.6665 5.90073 8.6665 7.00649C8.6665 8.11226 7.92031 9.00865 6.99984 9.00865C6.07936 9.00865 5.33317 8.11226 5.33317 7.00649Z" fill="#2DD790" />
         </svg>
