2a8fcfe0332a539a18dfe6d52133dc494e152c0e
jikwan <jikwan0327@gmail.com>
2024-01-27 17:12:19 +0900

    fix: styled 세팅

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,5 @@
-import { Inter } from "next/font/google";
-import { Providers } from './provider';
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import { Providers } from "./provider";
 
 export const metadata = {
   title: "Create Next App",
@@ -12,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <Providers>{children}</Providers>
       </body>
     </html>
