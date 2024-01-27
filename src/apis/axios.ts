4071de76c2110ff4292790313e9d8995893e2964
jikwan <jikwan0327@gmail.com>
2024-01-28 05:21:56 +0900

    instance request

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -1,6 +1,22 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 export const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   timeout: 10000,
 });
+
+instance.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.get("access_token");
+    const returnConfig = {
+      ...config,
+    };
+    if (accessToken) {
+      returnConfig.headers!["Authorization"] = `Bearer ${accessToken}`;
+    }
+    return returnConfig;
+  },
+  (error: AxiosError) => {
+    return Promise.reject(error);
+  }
+);
