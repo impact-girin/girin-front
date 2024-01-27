2117205f3c9e65b3db45ec819bf0e81c70864ce4
Yeong joon <bluehome8626@naver.com>
2024-01-27 17:19:15 +0900

    feat: 전화번호 입력, 인증번호 입력 페이지

---
+++ src/app/signup/certified/page.jsx
@@ -0,0 +1,113 @@
+"use client";
+
+import {
+  Button,
+  Container,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Text,
+} from "@chakra-ui/react";
+import Image from "next/image";
+import Link from "next/link";
+import LeftIcon from "../../../assets/back_button.png";
+import { useEffect, useState } from "react";
+
+const Certified = () => {
+  const [time, setTime] = useState(180); // 남은 시간 (단위: 초)
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime((prev) => prev - 1);
+    }, 1000);
+    return () => clearInterval(timer);
+  }, [time]);
+
+  const getSeconds = (time) => {
+    const seconds = Number(time % 60);
+    if (seconds < 10) {
+      return "0" + String(seconds);
+    } else {
+      return String(seconds);
+    }
+  };
+
+  return (
+    <Container height={"100vh"}>
+      <Flex
+        flexDir="column"
+        height={"100%"}
+        padding={"19px"}
+        justifyContent={"space-around"}
+      >
+        <Link href={"/signup/phone"}>
+          <Image
+            width={8}
+            height={8}
+            style={{ marginBottom: "36px", marginBottom: "19px" }}
+            src={LeftIcon}
+            alt=""
+          />
+        </Link>
+        <Text
+          fontSize={"26px"}
+          lineHeight={"26px"}
+          fontWeight={"bold"}
+          marginBottom={"32px"}
+        >
+          인증번호를 입력해주세요
+        </Text>
+        <Flex>
+          <FormControl>
+            <FormLabel>인증번호</FormLabel>
+            <InputGroup>
+              <Input
+                padding={"18px 15px"}
+                lineHeight={"17px"}
+                fontSize={"16px"}
+                border="1px solid"
+                borderColor="#E0E0E1"
+                width={"100%"}
+                placeholder="6자리 숫자"
+              />
+              <InputRightElement width="90px">
+                <Text
+                  marginRight={"15px"}
+                  fontSize="12px"
+                  fontWeight={400}
+                  color="#70757D"
+                >
+                  <span>
+                    {parseInt(time / 60)}:{getSeconds(time)}
+                  </span>
+                </Text>
+                <Text
+                  color="rgba(50, 129, 246, 0.80)"
+                  fontSize="13px"
+                  fontWeight={400}
+                  textDecor="underline"
+                  marginRight={"5px"}
+                >
+                  재전송
+                </Text>
+              </InputRightElement>
+            </InputGroup>
+          </FormControl>
+        </Flex>
+        <Button
+          marginTop={"auto"}
+          height={"50px"}
+          background={"#2DD790"}
+          color={"white"}
+          width={"100%"}
+        >
+          확인
+        </Button>
+      </Flex>
+    </Container>
+  );
+};
+
+export default Certified;
---
+++ src/app/signup/phone/page.jsx
@@ -0,0 +1,103 @@
+"use client";
+
+import {
+  Button,
+  Container,
+  Flex,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Input,
+  Text,
+} from "@chakra-ui/react";
+import Image from "next/image";
+import LeftIcon from "../../../assets/back_button.png";
+import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+const Phone = () => {
+  const [value, setValue] = useState("");
+  const [errorNumber, setErrorNumber] = useState(false);
+
+  const navigate = useRouter();
+
+  const checkNumber = () => {
+    const regex = /^01[016789]\d{3,4}\d{4}$/;
+    if (regex.test(value)) {
+      setErrorNumber(false); // 일치할 경우 에러가 아닌 것으로 설정
+      navigate.push("/signup/certified");
+    } else {
+      setErrorNumber(true); // 일치하지 않을 경우 에러로 설정
+    }
+  };
+
+  const onChangeInput = (value) => {
+    setValue(String(value));
+  };
+
+  return (
+    <Container height={"100vh"}>
+      <Flex
+        flexDir="column"
+        height={"100%"}
+        padding={"19px"}
+        justifyContent={"space-around"}
+      >
+        <Link href={"/"}>
+          <Image
+            width={8}
+            height={8}
+            style={{ marginBottom: "36px", marginBottom: "19px" }}
+            src={LeftIcon}
+            alt=""
+          />
+        </Link>
+        <Text
+          fontSize={"26px"}
+          lineHeight={"26px"}
+          fontWeight={"bold"}
+          marginBottom={"32px"}
+        >
+          휴대폰번호를 입력해주세요
+        </Text>
+        <Flex>
+          <FormControl>
+            <FormLabel>휴대폰번호</FormLabel>
+            <Input
+              onChange={(e) => onChangeInput(e.target.value)}
+              padding={"18px 15px"}
+              lineHeight={"17px"}
+              fontSize={"16px"}
+              border="1px solid"
+              borderColor={errorNumber ? "#CC4363" : "#E0E0E1"}
+              width={"100%"}
+              placeholder="휴대폰번호를 입력해주세요"
+            />
+            {errorNumber && (
+              <FormHelperText
+                margin="0 0 0 10px"
+                color="#CC4363"
+                fontSize="11px"
+              >
+                휴대폰번호를 다시 한 번 확인해주세요
+              </FormHelperText>
+            )}
+          </FormControl>
+        </Flex>
+        <Button
+          marginTop={"auto"}
+          height={"50px"}
+          background={"#2DD790"}
+          color={"white"}
+          width={"100%"}
+          onClick={() => checkNumber()}
+        >
+          다음으로
+        </Button>
+      </Flex>
+    </Container>
+  );
+};
+
+export default Phone;
